792b285a9f3c60b5a02323571a53ab80e2306402
Adil <react631@gmail.com>
2023-11-02 03:02:50 -0700

    delete main function

--- trello.js
+++ trello.js
@@ -77,8 +77,13 @@ function addSubTask(mainTaskIndex) {
 }
 // for deleting sub task
 function deleteSubTask(mainTaskIndex, subTaskIndex) {
-  AllTasks[mainTaskIndex].subTasks.splice(subTaskIndex, 1); 
-  displayTasks(); 
+  AllTasks[mainTaskIndex].subTasks.splice(subTaskIndex, 1);
+  displayTasks();
+}
+// for deleting main task
+function deleteMainTask(mainTaskIndex) {
+  AllTasks.splice(mainTaskIndex, 1);
+  displayTasks();
 }
 
 function displayTasks() {
@@ -89,6 +94,56 @@ function displayTasks() {
     const task = document.createElement("div");
     task.classList.add("task");
     mainTaskList.appendChild(task);
+    // operation btns main task
+    const mainbtndiv = document.createElement("div");
+    mainbtndiv.classList.add("main-div-btns");
+    const mainmodel = document.createElement("div");
+    mainmodel.classList.add("main-model");
+    task.appendChild(mainbtndiv);
+
+    // main model contents
+    const dialog = document.createElement("div");
+    const maininputedit = document.createElement("input");
+    const updatebtn = document.createElement("button");
+    dialog.classList.add("model-div");
+    maininputedit.classList.add("main-task-input");
+    updatebtn.classList.add("edit-main-task");
+    updatebtn.innerText = "Update";
+
+    // delete and edit main task btn
+    const mainTaskDelete = document.createElement("button");
+    const editMainTaskBtn = document.createElement("button");
+    mainTaskDelete.classList.add("main-task-delete-btn");
+    editMainTaskBtn.classList.add("edit-main-task");
+    mainTaskDelete.innerHTML = "x";
+    editMainTaskBtn.innerText = "Edit Main Task";
+    mainbtndiv.appendChild(mainTaskDelete);
+    mainbtndiv.appendChild(editMainTaskBtn);
+
+    // add eventlistener to delete main task
+    mainTaskDelete.addEventListener("click", function () {
+      deleteMainTask(index);
+    });
+
+    //add eventlistener to edit main task
+    editMainTaskBtn.addEventListener("click", () => {
+      openEditMainTaskForm(index, mainTask);
+      task.appendChild(mainmodel);
+      mainmodel.appendChild(dialog);
+      dialog.appendChild(maininputedit);
+      maininputedit.value = mainTask.text;
+      dialog.appendChild(updatebtn);
+      mainmodel.style.display = "flex";
+    });
+
+    function openEditMainTaskForm(mainTaskIndex, mainTask) {
+      // update main text add evenlistener
+      updatebtn.addEventListener("click", function () {
+        AllTasks[mainTaskIndex].text = maininputedit.value;
+        mainmodel.style.display = "none";
+        displayTasks();
+      });
+    }
 
     const maintitle = document.createElement("h3");
     maintitle.classList.add("task-title");
@@ -172,24 +227,86 @@ function displayTasks() {
       const btndiv = document.createElement("div");
       btndiv.classList.add("operation-btns");
       const deletebtn = document.createElement("button");
+      const editbtn = document.createElement("button");
       deletebtn.innerText = "Delete";
-      deletebtn.classList.add('delete-sub-task')
+      editbtn.innerHTML = "Update";
+      deletebtn.classList.add("delete-sub-task");
+      editbtn.classList.add("delete-sub-task");
+      // sub task model
+      const subtaskmodel = document.createElement("div");
+      const subtaskdialog = document.createElement("div");
+      const subtaskinput1 = document.createElement("input");
+      const subtaskinput3 = document.createElement("input");
+      const subtaskinput2 = document.createElement("select");
+      const option1edit = document.createElement("option");
+      const option2edit = document.createElement("option");
+      const option3edit = document.createElement("option");
+      
+      const updatesubtask = document.createElement("button");
+
+      updatesubtask.innerHTML = "Update";
+      subtaskinput1.value = subTask.subinput1;
+      subtaskinput2.value = subTask.subinput2;
+      subtaskinput3.value = subTask.subinput3;
+      option1edit.value = "Select Name";
+      option2edit.value = "Asad";
+      option3edit.value = "Dawood";
+      option1edit.innerHTML = option3edit.value;
+      option2edit.innerHTML = option2edit.value;
+      option3edit.innerHTML = option1edit.value;
+
       subTaskItem.classList.add("paragraph");
       subTaskItemSelect.classList.add("paragraph");
       subTaskItemAssign.classList.add("paragraph");
+      subtaskmodel.classList.add("sub-task-input-model");
+      subtaskdialog.classList.add("sub-task-dialog");
       subTaskItem.innerHTML = "Task Description : " + subTask.subinput1;
       subTaskItemSelect.innerHTML = "Task Assign To : " + subTask.subinput2;
       subTaskItemAssign.innerHTML = "Task Name : " + subTask.subinput3;
+      subtaskinput1.classList.add("sub-input");
+      subtaskinput2.classList.add("sub-input");
+      subtaskinput3.classList.add("sub-input");
+      updatesubtask.classList.add("delete-sub-task");
       listforItem.appendChild(subTaskItem);
       listforItem.appendChild(subTaskItemSelect);
       listforItem.appendChild(subTaskItemAssign);
       listforItem.appendChild(btndiv);
       btndiv.appendChild(deletebtn);
+      btndiv.appendChild(editbtn);
 
       // Event listener to delete the sub-task
       deletebtn.addEventListener("click", () => {
+       
         deleteSubTask(index, subTaskIndex);
+        displayTasks();
       });
+
+      // Event listener to edit the sub-task
+      editbtn.addEventListener("click", function () {
+        listforItem.appendChild(subtaskmodel);
+        subtaskmodel.style.display = "flex";
+        subtaskmodel.appendChild(subtaskdialog);
+        subtaskdialog.appendChild(subtaskinput1);
+        subtaskdialog.appendChild(subtaskinput2);
+        subtaskdialog.appendChild(subtaskinput3);
+        subtaskdialog.appendChild(updatesubtask);
+        subtaskinput2.appendChild(option1edit);
+        subtaskinput2.appendChild(option2edit);
+        subtaskinput2.appendChild(option3edit);
+        // function calling here
+        openEditSubTaskForm(index, subTaskIndex, subTask);
+      });
+
+      // edit sub task function
+      function openEditSubTaskForm(mainTaskIndex, subTaskIndex, subTask) {
+        updatesubtask.addEventListener('click',function () {
+          AllTasks[mainTaskIndex].subTasks[subTaskIndex].subinput1 = subtaskinput1.value;
+          AllTasks[mainTaskIndex].subTasks[subTaskIndex].subinput2 = subtaskinput2.value;
+          AllTasks[mainTaskIndex].subTasks[subTaskIndex].subinput3 = subtaskinput3.value;
+          subtaskmodel.style.display = "none";
+          displayTasks()
+        })
+      }
     });
 
     // close model
